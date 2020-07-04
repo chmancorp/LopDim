51fed6a7ac514d116806ce46c41d00ea0ef98815
chmancorp <morris400jul@gmail.com>
2020-07-04 10:20:05 -0500

    feat(navbar): add navbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
+  exports:[MatToolbarModule,
+  MatIconModule],
   declarations: [],
   imports: [
     CommonModule
